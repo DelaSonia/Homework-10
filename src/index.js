f83af149d39ed457d596099b7ef546378d226fcc
DelaSonia <dela.sonia58@gmail.com>
2024-07-01 13:46:00 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt("Hello, What's your name");
+let age = prompt("How Old Are You?");
+let email = prompt("Kindly provide your email?");
+alert (`Hi ${name} you are ${age} years old and we will keep in touch via your email "${email}"`)
